8a599355e4e5b5d46f4644f560e8507021fe4182
Nick <nick@nicks-mbp.home>
2017-01-11 12:10:12 +0000

    UI for main menu - whole screen swipes not yet recognised

--- UnitConverter/ViewController.swift
+++ UnitConverter/ViewController.swift
@@ -10,17 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var buttons = [UIImageView]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Hello World!")
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidLoad()
+        
+        var contentWidth: CGFloat = 0.0
+        
+        let screenSize = UIScreen.main.bounds
+        let screenWidth = screenSize.width
+        let screenHeight = screenSize.height
+        
+        scrollView.frame.size.width = 3 * (screenWidth / 5)
+        scrollView.frame.size.height = screenHeight
+        scrollView.center = CGPoint(x: screenWidth / 2, y: screenHeight / 2)
+        let scrollWidth = scrollView.frame.size.width
+        
+        
+        for x in 0...5 {
+            let image = UIImage(named: "icon\(x)")
+            let buttonView = UIImageView(image: image)
+            buttons.append(buttonView)
+            
+            var newX: CGFloat = 0.0
+            newX = (scrollWidth / 2) + scrollWidth * CGFloat(x)
+            contentWidth += scrollWidth
+            
+            scrollView.addSubview(buttonView)
+            
+            buttonView.frame = CGRect(x: newX - (screenWidth / 6), y: scrollView.frame.size.height / 2 - (screenWidth / 6), width: screenWidth / 3, height: screenWidth / 3)
+        }
+        
+        scrollView.clipsToBounds = false
+        
+        scrollView.contentSize = CGSize(width: contentWidth, height: scrollView.frame.size.height)
+        
     }
-
 
 }
 
